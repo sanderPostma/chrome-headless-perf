ed440b0e22d7e8fd7e559f82e78070f5f77b3a30
sanderPostma <spostma@sphereon.com>
2023-02-21 09:12:16 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,51 @@
+use std::{env};
+use std::collections::HashMap;
+use std::ffi::OsStr;
+
+
+use chrono::{Datelike, Timelike, Utc, Weekday};
+
+use headless_chrome::{Browser, LaunchOptionsBuilder};
+
+use num_traits::FromPrimitive;
+
+
+pub fn main() {
+    let options = LaunchOptionsBuilder::default()
+        .args(vec![
+            &OsStr::new("--disable-gpu")])
+        .build()
+        .unwrap();
+    let browser = Browser::new(options).unwrap();
+    let tab = browser.new_tab().unwrap();
+
+    // Navigate to the page
+    let response = tab.navigate_to("https://www.winrate.io/volatility-index").unwrap();
+
+    // Wait for the table to be present
+    let start1 = Utc::now();
+    let table = tab.wait_for_elements_by_xpath("//table[@id='table']").unwrap();
+    println!("wait_for_elements_by_xpath took {}ms", (Utc::now().timestamp_millis() - start1.timestamp_millis()));
+
+    for table_elem in table {
+        let start2 = Utc::now();
+        match table_elem.find_elements_by_xpath("//tbody/tr/td") {
+            Ok(elements) => {
+                println!("  find_elements_by_xpath took {}ms", (Utc::now().timestamp_millis() - start2.timestamp_millis()));
+                for el in elements {
+                    let start3 = Utc::now();
+                    match el.get_inner_text() {
+                        Ok(text) => {
+                            println!("text: {}", text);
+                        }
+                        Err(_) => {}
+                    }
+                    println!("  get_inner_text took {}ms", (Utc::now().timestamp_millis() - start3.timestamp_millis()));
+                }
+            }
+            Err(err) => { println!("could not find table: {}", err) }
+        }
+    }
+    println!("total scan took {}ms", (Utc::now().timestamp_millis() - start1.timestamp_millis()));
+    response.close(true).unwrap();
+}
